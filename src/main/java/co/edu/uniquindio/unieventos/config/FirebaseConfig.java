db1f2db562f038be3e81ed3783be7dcab47ae14f
Luisc00 <luisc.moralesc@uqvirtual.edu.co>
2024-11-17 00:53:13 -0500

    Cambios firebase

--- src/main/java/co/edu/uniquindio/unieventos/config/FirebaseConfig.java
+++ src/main/java/co/edu/uniquindio/unieventos/config/FirebaseConfig.java
@@ -5,28 +5,36 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
+
     @Bean
     public FirebaseApp intializeFirebase() throws IOException {
-        // Cargar el archivo desde el classpath
-        InputStream serviceAccount = new ClassPathResource("programacion-a5156-firebase-adminsdk-j2c6w-00a0167936.json").getInputStream();
+        // Obtener la ruta del archivo JSON desde la variable de entorno
+        String credentialsPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
 
+        // Verificar si la variable de entorno está configurada correctamente
+        if (credentialsPath == null || credentialsPath.isEmpty()) {
+            throw new IOException("La variable de entorno GOOGLE_APPLICATION_CREDENTIALS no está configurada.");
+        }
+
+        // Cargar el archivo JSON utilizando la ruta de la variable de entorno
+        FileInputStream serviceAccount = new FileInputStream(credentialsPath);
+
+        // Configuración de Firebase
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setStorageBucket("programacion-a5156.appspot.com")
                 .build();
 
+        // Inicializar Firebase
         if (FirebaseApp.getApps().isEmpty()) {
             return FirebaseApp.initializeApp(options);
         }
         return null;
     }
 }
-
