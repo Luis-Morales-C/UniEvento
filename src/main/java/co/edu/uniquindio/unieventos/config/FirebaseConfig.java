936e385539e3a877130f1ed856a73431a33102e2
Luisc00 <luisc.moralesc@uqvirtual.edu.co>
2024-11-17 00:14:26 -0500

    Deploy

--- src/main/java/co/edu/uniquindio/unieventos/config/FirebaseConfig.java
+++ src/main/java/co/edu/uniquindio/unieventos/config/FirebaseConfig.java
@@ -5,24 +5,28 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
-public class FirebaseConfig{
+public class FirebaseConfig {
     @Bean
     public FirebaseApp intializeFirebase() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream(
-                "src/main/resources/programacion-a5156-firebase-adminsdk-j2c6w-00a0167936.json"
-        );
+        // Cargar el archivo desde el classpath
+        InputStream serviceAccount = new ClassPathResource("programacion-a5156-firebase-adminsdk-j2c6w-00a0167936.json").getInputStream();
+
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setStorageBucket("programacion-a5156.appspot.com")
                 .build();
 
-        if(FirebaseApp.getApps().isEmpty()) {
+        if (FirebaseApp.getApps().isEmpty()) {
             return FirebaseApp.initializeApp(options);
         }
         return null;
     }
 }
+
